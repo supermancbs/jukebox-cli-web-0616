5f11607da0de743b781824ec25dc91f438d56064
supermancbs <clark.sacktor@gmail.com>
2016-05-01 20:03:19 -0400

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -1,15 +1,16 @@
+
 #Here is the song hash you will be working with. Each key is a song name and each value is the location of it's mp3 file.
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
 # my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
+ #"Go Go GO" => '~/Desktop/learn/jukebox-cli-web-0616/audio/Emerald-Park/01.mp3',
+ #"LiberTeens" => '~/Desktop/learn/jukebox-cli-web-0616/audio/Emerald-Park/02.mp3',
+ #"Hamburg" =>  '~/Desktop/learn/jukebox-cli-web-0616/audio/Emerald-Park/03.mp3',
+ #"Guiding Light" => '~/Desktop/learn/jukebox-cli-web-0616/audio/Emerald-Park/04.mp3',
+ #"Wolf" => '~/Desktop/learn/jukebox-cli-web-0616/audio/Emerald-Park/05.mp3',
+ #"Blue" => '~/Desktop/learn/jukebox-cli-web-0616/audio/Emerald-Park/06.mp3',
+ #"Graduation Failed" => '~/Desktop/learn/jukebox-cli-web-0616/audio/Emerald-Park/07.mp3'
 # }
 
 def help
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,4 @@
+require "pry"
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +11,59 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+output = "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+	puts output
+
+end 
+
+def list(songs)
+	songs.each_with_index do |song, index|
+		puts "#{index+1}. #{song}"
+	end 
+end 
+
+def play(songs)
+	puts "Please enter a song name or number:"
+	response = gets.chomp 
+	#binding.pry
+	#puts "Playing #{songs[response-1]}"
+	if (1...songs.length).to_a.include?(response.to_i)
+		puts "Playing #{songs[(response.to_i-1)]}"
+	elsif 
+		songs.each do |song|
+			if song==response 
+				puts "Playing #{song}"
+			end 
+		end
+	end 
+		puts "Invalid input, please try again"
+	  
+end 
+
+def exit_jukebox
+	puts "Goodbye"
+end 
+
+def run(songs)
+	help
+	puts "Please enter a command:"
+	while true
+		response=gets.chomp
+		if response=="help"
+			help
+		elsif response=="list"
+			list(songs)
+		elsif response=="play"
+			play(songs)
+		elsif response=="exit"
+			exit
+			break
+		end 
+	end 
+
+end 
